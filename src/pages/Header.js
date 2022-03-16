c4626a680693722ff7a32207e464138ed72de0f3
leonardo-terra <leo.terra.melo@gmail.com>
2022-03-16 13:10:01 -0300

    header-design

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Wallet() {
   const email = useSelector((state) => state.user.email);
   const exp = useSelector((state) => state.wallet.expenses);
-  const cambio = 'BRL';
 
   // Reduce feito com consulta ao PR do companheiro Rafael França.
-  const updateTotalExpense = (expenses) => expenses
-    .reduce((acc, { value, currency, exchangeRates }) => {
+  const updateTotalExpense = (expenses) =>
+    expenses.reduce((acc, { value, currency, exchangeRates }) => {
       acc += Number(value) * Number(exchangeRates[currency].ask);
       return acc;
     }, 0);
 
   return (
-    <>
-      <span data-testid="email-field">{ `email: ${email}` }</span>
-      <br />
-      <span data-testid="header-currency-field">{ `Cambio utilizado: ${cambio}` }</span>
-      <br />
-      <span
-        data-testid="total-field"
-      >
-        { `Total de gastos:  ${updateTotalExpense(exp)}` }
-
+    <div className="flex p-2 justify-around  bg-emerald-700 text-white font-bold">
+      <h1 className="text-2xl">TrybeWallet</h1>
+      <span data-testid="email-field">{`e-mail: ${email}`}</span>
+      <span data-testid="total-field">
+        {`Total de despesas: R$${updateTotalExpense(exp)} BRL`}
       </span>
-    </>
+    </div>
   );
 }
